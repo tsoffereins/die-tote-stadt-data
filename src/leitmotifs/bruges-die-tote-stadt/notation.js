c4dd13e1692368dc1ac3eb820baa43dc850fd175
Thomas Offereins <thomas.offereins@coolblue.nl>
2021-05-18 23:07:23 +0200

    Change the notation for the motif 'die tote stadt'

--- src/leitmotifs/bruges-die-tote-stadt/notation.js
+++ src/leitmotifs/bruges-die-tote-stadt/notation.js
@@ -1,24 +1,27 @@
 (function () {
     let system;
 
+    const registry = new Vex.Flow.Registry();
+    Vex.Flow.Registry.enableDefaultRegistry(registry);
+
     // Configure the canvas
     const vf = new Vex.Flow.Factory({
         renderer: {
             elementId: 'bruges-die-tote-stadt',
-            width: 422,
+            width: 282,
             height: 120
         }
     });
 
     // Setup the score
     const score = vf.EasyScore();
-    score.set({ time: '2/2' });
+    score.set({ time: '4/4' });
 
     // Measure 1
     system = vf.System({
         x: 0,
         y: 0,
-        width: 260,
+        width: 280,
         spaceBetweenStaves: 10
     });
 
@@ -26,38 +29,40 @@
         .addStave({
             voices: [
                 score.voice(
-                    score.notes(
-                        [
-                            'B4/q/r',
-                            'B4/8/r',
-                            'D4/8',
-                            'A4/q',
-                            'B4/8/r',
-                            'F#4/8'
-                        ].join(',')
-                    )
+                    [
+                        score.notes(
+                            [
+                                'B4/8/r',
+                                'F4/8'
+                            ].join(',')
+                        ),
+                        score.tuplet(
+                            score.notes(
+                                [
+                                    'C5/q',
+                                    'A4/8'
+                                ].join(',')
+                            ),
+                            { num_notes: 3 }
+                        ),
+                        score.notes(
+                            [
+                                'E5/q[id="n1"]',
+                                'E5/8[id="n2"]',
+                                'B4/8/r'
+                            ].join(',')
+                        )
+                    ].reduce((a, b) => a.concat(b))
                 )
             ]
         })
         .addClef('treble')
-        .addTimeSignature('2/2');
+        .addTimeSignature('4/4');
 
-    // Measure 2
-    system = vf.System({
-        x: 260,
-        y: 0,
-        width: 160,
-        spaceBetweenStaves: 10
+    vf.Curve({
+        from: registry.getElementById('n1'),
+        to: registry.getElementById('n2')
     });
-
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    score.notes('C5/w')
-                )
-            ]
-        });
 
     // Render the score
     vf.draw();
