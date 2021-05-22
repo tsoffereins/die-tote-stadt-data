3993f08340a72bff38668e8580d9166331bca5c0
Thomas Offereins <thomas@mybit.nl>
2021-05-22 17:02:35 +0200

    Use the new notation helper for the motif 'Vergangenheit'

--- src/leitmotifs/vergangenheit/notation.js
+++ src/leitmotifs/vergangenheit/notation.js
@@ -1,83 +1,27 @@
 (function () {
-    let system;
-
-    // Configure the canvas
-    const vf = new Vex.Flow.Factory({
-        renderer: {
-            elementId: 'vergangenheit',
-            width: 522,
-            height: 120
-        }
+    const { score, measure, notes, beam, render } = new VF.Sheet({
+        id: 'vergangenheit',
+        measureWidths: [280, 240]
     });
 
-    // Setup the score
-    const score = vf.EasyScore();
     score.set({ time: '4/4' });
 
     // Measure 1
-    system = vf.System({
-        x: 0,
-        y: 0,
-        width: 280,
-        spaceBetweenStaves: 10
-    });
-
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    [
-                        score.notes(
-                            [
-                                'B4/h/r',
-                                'B4/q/r',
-                                'B4/8/r'
-                            ].join(',')
-                        ),
-                        score.beam(
-                            score.notes(
-                                [
-                                    'B4/16',
-                                    'F#4/16'
-                                ].join(','),
-                                { stem: 'up' }
-                            )
-                        )
-                    ].reduce((a, b) => a.concat(b))
-                )
-            ]
-        })
+    measure()
+        .addNotes([
+            notes('B4/h/r', 'B4/q/r', 'B4/8/r'),
+            beam(notes('B4/16', 'F#4/16', { stem: 'up' }))
+        ])
         .addClef('treble')
         .addTimeSignature('4/4');
 
     // Measure 2
-    system = vf.System({
-        x: 280,
-        y: 0,
-        width: 240,
-        spaceBetweenStaves: 10
-    });
+    measure()
+        .addNotes([
+            notes('E4/q.'),
+            beam(notes('F#4/16', 'C#4/16')),
+            notes('G#4/h')
+        ]);
 
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    [
-                        score.notes('E4/q.'),
-                        score.beam(
-                            score.notes(
-                                [
-                                    'F#4/16',
-                                    'C#4/16'
-                                ].join(',')
-                            )
-                        ),
-                        score.notes('G#4/h')
-                    ].reduce((a, b) => a.concat(b))
-                )
-            ]
-        });
-
-    // Render the score
-    vf.draw();
+    render();
 })();
