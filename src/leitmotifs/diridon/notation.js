19ed71f58f767e108980d21605c0306f57405144
Thomas Offereins <thomas@mybit.nl>
2021-05-22 14:51:28 +0200

    Use the new notation helper for the motif 'Diridon'

--- src/leitmotifs/diridon/notation.js
+++ src/leitmotifs/diridon/notation.js
@@ -1,202 +1,53 @@
 (function () {
-    let system;
-
-    const registry = new Vex.Flow.Registry();
-    Vex.Flow.Registry.enableDefaultRegistry(registry);
-
-    // Configure the canvas
-    const vf = new Vex.Flow.Factory({
-        renderer: {
-            elementId: 'diridon',
-            width: 1220,
-            height: 120
-        }
+    const { score, measure, notes, find, render } = new VF.Sheet({
+        id: 'diridon',
+        measureWidths: [300, 220, 220, 260, 220]
     });
 
-    // Setup the score
-    const score = vf.EasyScore();
     score.set({ time: '4/4' });
 
     // Measure 1
-    system = vf.System({
-        x: 0,
-        y: 0,
-        width: 300,
-        spaceBetweenStaves: 10
-    });
-
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    [
-                        score.notes(
-                            [
-                                'B4/h/r',
-                                'B4/q/r'
-                            ].join(',')
-                        ),
-                        score.beam(
-                            score.notes(
-                                [
-                                    'C5/8',
-                                    'D5/8'
-                                ].join(',')
-                            )
-                        )
-                    ].reduce((a, b) => a.concat(b))
-                )
-            ]
-        })
+    measure()
+        .addNotes([
+            notes('B4/h/r', 'B4/q/r'),
+            beam(notes('C5/8', 'D5/8'))
+        ])
         .addClef('treble')
         .addKeySignature('Ab')
         .addTimeSignature('4/4');
 
     // Measure 2
-    system = vf.System({
-        x: 300,
-        y: 0,
-        width: 220,
-        spaceBetweenStaves: 10
-    });
-
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    [
-                        score.notes('E5/q'),
-                        score.beam(
-                            score.notes(
-                                [
-                                    'D5/8',
-                                    'C5/8'
-                                ].join(',')
-                            )
-                        ),
-                        score.notes('B4/q'),
-                        score.beam(
-                            score.notes(
-                                [
-                                    'C5/8',
-                                    'D5/8'
-                                ].join(',')
-                            )
-                        )
-                    ].reduce((a, b) => a.concat(b))
-                )
-            ]
-        });
+    measure()
+        .addNotes([
+            notes('E5/q'),
+            beam(notes('D5/8', 'C5/8')),
+            notes('B4/q'),
+            beam(notes('C5/8', 'D5/8'))
+        ]);
 
     // Measure 3
-    system = vf.System({
-        x: 520,
-        y: 0,
-        width: 220,
-        spaceBetweenStaves: 10
-    });
-
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    [
-                        score.notes(
-                            [
-                                'E5/q.',
-                                'G5/8',
-                                'A4/4'
-                            ].join(',')
-                        ),
-                        score.beam(
-                            score.notes(
-                                [
-                                    'B4/8',
-                                    'C5/8'
-                                ].join(',')
-                            )
-                        )
-                    ].reduce((a, b) => a.concat(b))
-                )
-            ]
-        });
+    measure()
+        .addNotes([
+            notes('E5/q.', 'G5/8', 'A4/4'),
+            beam(notes('B4/8', 'C5/8'))
+        ]);
 
     // Measure 4
-    system = vf.System({
-        x: 740,
-        y: 0,
-        width: 260,
-        spaceBetweenStaves: 10
-    });
+    measure()
+        .addNotes([
+            beam(notes('B4/8.', 'C5/16')),
+            beam(notes('D5/8[id="n1"]', 'E5/8')),
+            beam(notes('F5/8.', 'G5/16')),
+            beam(notes('A5/8', 'B5/8'))
+        ]);
 
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    [
-                        score.beam(
-                            score.notes(
-                                [
-                                    'B4/8.',
-                                    'C5/16'
-                                ].join(',')
-                            )
-                        ),
-                        score.beam(
-                            score.notes(
-                                [
-                                    'D5/8[id="n1"]',
-                                    'E5/8'
-                                ].join(',')
-                            )
-                        ),
-                        score.beam(
-                            score.notes(
-                                [
-                                    'F5/8.',
-                                    'G5/16'
-                                ].join(',')
-                            )
-                        ),
-                        score.beam(
-                            score.notes(
-                                [
-                                    'A5/8',
-                                    'B5/8'
-                                ].join(',')
-                            )
-                        )
-                    ].reduce((a, b) => a.concat(b))
-                )
-            ]
-        });
-
-    registry.getElementById('n1')
-        .addAccidental(0, new Vex.Flow.Accidental('n'));
+    find('n1').addAccidental(0, new Vex.Flow.Accidental('n'));
 
     // Measure 5
-    system = vf.System({
-        x: 1000,
-        y: 0,
-        width: 220,
-        spaceBetweenStaves: 10
-    });
+    measure()
+        .addNotes([
+            score.notes('G5/q', 'F5/q', 'E5/h')
+        ]);
 
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    score.notes(
-                        [
-                            'G5/q',
-                            'F5/q',
-                            'E5/h',
-                        ].join(',')
-                    )
-                )
-            ]
-        });
-
-    // Render the score
-    vf.draw();
+    render();
 })();
