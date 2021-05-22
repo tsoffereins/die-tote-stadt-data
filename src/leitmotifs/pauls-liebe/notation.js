76440e4f7f13c80dfcc2aab6d181c424ac056b2a
Thomas Offereins <thomas@mybit.nl>
2021-05-22 16:36:56 +0200

    Use the new notation helper for the motif 'Pauls Liebe'

--- src/leitmotifs/pauls-liebe/notation.js
+++ src/leitmotifs/pauls-liebe/notation.js
@@ -1,115 +1,36 @@
 (function () {
-    let system;
-
-    const registry = new Vex.Flow.Registry();
-    Vex.Flow.Registry.enableDefaultRegistry(registry);
-
-    // Configure the canvas
-    const vf = new Vex.Flow.Factory({
-        renderer: {
-            elementId: 'pauls-liebe',
-            width: 682,
-            height: 120
-        }
+    const { score, measure, notes, beam, curve, render } = new VF.Sheet({
+        id: 'pauls-liebe',
+        measureWidths: [320, 180, 180]
     });
 
-    // Setup the score
-    const score = vf.EasyScore();
     score.set({ time: '2/2' });
 
     // Measure 1
-    system = vf.System({
-        x: 0,
-        y: 0,
-        width: 320,
-        spaceBetweenStaves: 10
-    });
-
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    [
-                        score.beam(
-                            score.notes(
-                                [
-                                    'G4/8',
-                                    'Ab4/16',
-                                    'Bb4/16'
-                                ].join(','),
-                                { stem: 'up' }
-                            )
-                        ),
-                        score.notes(
-                            [
-                                'A4/q.',
-                                'G4/8',
-                                'B4/q[id="n1"]'
-                            ].join(',')
-                        )
-                    ].reduce((a, b) => a.concat(b))
-                )
-            ]
-        })
+    measure()
+        .addNotes([
+                beam(notes('G4/8', 'Ab4/16', 'Bb4/16', { stem: 'up' })),
+                notes('A4/q.', 'G4/8', 'B4/q[id="n1"]')
+            ])
         .addClef('treble')
         .addTimeSignature('2/2');
 
     // Measure 2
-    system = vf.System({
-        x: 320,
-        y: 0,
-        width: 180,
-        spaceBetweenStaves: 10
-    });
+    measure()
+        .addNotes([
+            notes('B4/q[id="n2"]', 'C5/h', 'D5/q')
+        ]);
 
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    score.notes(
-                        [
-                            'B4/q[id="n2"]',
-                            'C5/h',
-                            'D5/q'
-                        ].join(',')
-                    )
-                )
-            ]
-        });
-
-    vf.Curve({
-        from: registry.getElementById('n1'),
-        to: registry.getElementById('n2')
-    });
+    curve('n1', 'n2');
 
     // Measure 3
-    system = vf.System({
-        x: 500,
-        y: 0,
-        width: 180,
-        spaceBetweenStaves: 10
-    });
+    measure()
+        .addNotes([
+            notes('G5/h[id="n3"]', 'G5/q[id="n4"]', 'B4/q/r')
+        ]);
 
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    score.notes(
-                        [
-                            'G5/h[id="n3"]',
-                            'G5/q[id="n4"]',
-                            'B4/q/r'
-                        ].join(',')
-                    )
-                )
-            ]
-        });
-
-    vf.Curve({
-        from: registry.getElementById('n3'),
-        to: registry.getElementById('n4')
-    });
+    curve('n3', 'n4');
 
     // Render the score
-    vf.draw();
+    render();
 })();
