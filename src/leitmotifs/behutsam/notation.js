2c8f2554717435ad3f490cde6070056668525b67
Thomas Offereins <thomas@mybit.nl>
2021-05-22 13:11:21 +0200

    Use the new notation helper for the motif 'behutsam'

--- src/leitmotifs/behutsam/notation.js
+++ src/leitmotifs/behutsam/notation.js
@@ -1,68 +1,33 @@
 (function () {
-    let system;
-
-    // Configure the canvas
-    const vf = new Vex.Flow.Factory({
-        renderer: {
-            elementId: 'behutsam',
-            width: 422,
-            height: 120
-        }
+    const { score, measure, notes, render } = new VF.Sheet({
+        id: 'behutsam',
+        measureWidths: [240, 180]
     });
 
-    // Setup the score
-    const score = vf.EasyScore();
     score.set({ time: '3/4' });
 
     // Measure 1
-    system = vf.System({
-        x: 0,
-        y: 0,
-        width: 240,
-        spaceBetweenStaves: 10
-    });
-
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    score.notes(
-                        [
-                            'B4/q/r',
-                            'B4/8/r.',
-                            'F#4/16',
-                            'A4/q'
-                        ].join(',')
-                    )
-                )
-            ]
-        })
+    measure()
+        .addNotes([
+            notes(
+                'B4/q/r',
+                'B4/8/r.',
+                'F#4/16',
+                'A4/q'
+            )
+        ])
         .addClef('treble')
         .addTimeSignature('3/4');
 
     // Measure 2
-    system = vf.System({
-        x: 240,
-        y: 0,
-        width: 180,
-        spaceBetweenStaves: 10
-    });
+    measure()
+        .addNotes([
+            notes(
+                'E4/q',
+                'B4/q/r',
+                'B4/q/r'
+            )
+        ]);
 
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    score.notes(
-                        [
-                            'E4/q',
-                            'B4/q/r',
-                            'B4/q/r'
-                        ].join(',')
-                    )
-                )
-            ]
-        });
-
-    // Render the score
-    vf.draw();
+    render();
 })();
