d77af8905f50ddd9cc0c5ebb54b495a1dac7522d
Thomas Offereins <thomas@mybit.nl>
2021-05-22 14:40:57 +0200

    Use the new notation helper for the motif 'Die tote Stadt'

--- src/leitmotifs/die-tote-stadt/notation.js
+++ src/leitmotifs/die-tote-stadt/notation.js
@@ -1,69 +1,37 @@
 (function () {
-    let system;
-
-    const registry = new Vex.Flow.Registry();
-    Vex.Flow.Registry.enableDefaultRegistry(registry);
-
-    // Configure the canvas
-    const vf = new Vex.Flow.Factory({
-        renderer: {
-            elementId: 'die-tote-stadt',
-            width: 422,
-            height: 120
-        }
+    const { score, measure, notes, tuplet, curve, render } = new VF.Sheet({
+        id: 'die-tote-stadt',
+        measureWidths: [420]
     });
 
-    // Setup the score
-    const score = vf.EasyScore();
     score.set({ time: '4/4' });
 
     // Measure 1
-    system = vf.System({
-        x: 0,
-        y: 0,
-        width: 420,
-        spaceBetweenStaves: 10
-    });
-
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    [
-                        score.notes(
-                            [
-                                'B4/8/r',
-                                'F4/8'
-                            ].join(',')
-                        ),
-                        score.tuplet(
-                            score.notes(
-                                [
-                                    'C5/q',
-                                    'A4/8'
-                                ].join(',')
-                            ),
-                            { num_notes: 3 }
-                        ),
-                        score.notes(
-                            [
-                                'E5/q[id="n1"]',
-                                'E5/8[id="n2"]',
-                                'B4/8/r'
-                            ].join(',')
-                        )
-                    ].reduce((a, b) => a.concat(b))
+    measure()
+        .addNotes([
+            notes(
+                notes(
+                    'B4/8/r',
+                    'F4/8'
+                ),
+                tuplet(
+                    notes(
+                        'C5/q',
+                        'A4/8'
+                    ),
+                    { num_notes: 3 }
+                ),
+                notes(
+                    'E5/q[id="n1"]',
+                    'E5/8[id="n2"]',
+                    'B4/8/r'
                 )
-            ]
-        })
+            )
+        ])
         .addClef('treble')
         .addTimeSignature('4/4');
 
-    vf.Curve({
-        from: registry.getElementById('n1'),
-        to: registry.getElementById('n2')
-    });
+    curve('n1', 'n2');
 
-    // Render the score
-    vf.draw();
+    render();
 })();
