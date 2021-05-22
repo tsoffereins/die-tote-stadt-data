67cf4830df69c809fdf4a6429d2accd655d0ac3c
Thomas Offereins <thomas@mybit.nl>
2021-05-22 13:30:28 +0200

    Use the new notation helper for the motif 'alten lieb und treu'

--- src/leitmotifs/alten-lieb-und-treu/notation.js
+++ src/leitmotifs/alten-lieb-und-treu/notation.js
@@ -1,89 +1,41 @@
 (function () {
-    let system;
-
-    // Configure the canvas
-    const vf = new Vex.Flow.Factory({
-        renderer: {
-            elementId: 'alten-lieb-und-treu',
-            width: 642,
-            height: 120
-        }
+    const { score, measure, notes, render } = new VF.Sheet({
+        id: 'alten-lieb-und-treu',
+        measureWidths: [240, 200, 200]
     });
 
-    // Setup the score
-    const score = vf.EasyScore();
     score.set({ time: '4/4' });
 
     // Measure 1
-    system = vf.System({
-        x: 0,
-        y: 0,
-        width: 240,
-        spaceBetweenStaves: 10
-    });
-
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    score.notes(
-                        [
-                            'Eb5/h.',
-                            'Bb4/q'
-                        ].join(',')
-                    )
-                )
-            ]
-        })
+    measure()
+        .addNotes([
+            notes(
+                'Eb5/h.',
+                'Bb4/q'
+            )
+        ])
         .addClef('treble')
         .addTimeSignature('4/4');
 
     // Measure 2
-    system = vf.System({
-        x: 240,
-        y: 0,
-        width: 200,
-        spaceBetweenStaves: 10
-    });
-
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    score.notes(
-                        [
-                            'Ab4/q',
-                            'G4/q',
-                            'F4/q',
-                            'Eb4/q'
-                        ].join(',')
-                    )
-                )
-            ]
-        });
+    measure()
+        .addNotes([
+            notes(
+                'Ab4/q',
+                'G4/q',
+                'F4/q',
+                'Eb4/q'
+            )
+        ]);
 
     // Measure 3
-    system = vf.System({
-        x: 440,
-        y: 0,
-        width: 200,
-        spaceBetweenStaves: 10
-    });
+    measure()
+        .addNotes([
+            notes(
+                'F4/h.',
+                'B4/q/r'
+            )
+        ]);
 
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    score.notes(
-                        [
-                            'F4/h.',
-                            'B4/q/r'
-                        ].join(',')
-                    )
-                )
-            ]
-        });
-
-    // Render the score
-    vf.draw();
+    render();
 })();
