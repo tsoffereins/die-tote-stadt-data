ca861a7782e267b032e9d49170f155a9f391f81f
Thomas Offereins <thomas@mybit.nl>
2021-05-22 14:25:12 +0200

    Use the new notation helper for the motif 'Bells 2'

--- src/leitmotifs/bells-1/notation.js
+++ src/leitmotifs/bells-1/notation.js
@@ -1,89 +1,52 @@
 (function () {
-    let system;
-
-    // Configure the canvas
-    const vf = new Vex.Flow.Factory({
-        renderer: {
-            elementId: 'bells-1',
-            width: 742,
-            height: 160
-        }
+    const { score, measure, notes, render } = new VF.Sheet({
+        id: 'bells-1',
+        measureWidths: [440, 300],
+        height: 160
     });
 
-    // Setup the score
-    const score = vf.EasyScore();
     score.set({ time: '4/4' });
 
     // Measure 1
-    system = vf.System({
-        x: 0,
-        y: 30,
-        width: 440,
-        spaceBetweenStaves: 10
-    });
-
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    score.notes(
-                        [
-                            'B5/8/r',
-                            '(E5 B5 E6)/8',
-                            'B5/8/r',
-                            '(C#5 F#5 C#6)/8',
-                            'B5/8/r',
-                            '(E5 B5 E6)/8',
-                            'B5/8/r',
-                            '(C#5 F#5 C#6)/8'
-                        ].join(','),
-                        { stem: 'up' }
-                    )
-                ),
-                score.voice(
-                    score.notes(
-                        [
-                            'E4/q/r',
-                            '(C#4 F#4 C#5)/8',
-                            'E4/8/r',
-                            '(E4 B4 E5)/8',
-                            'E4/8/r',
-                            '(C#4 F#4 C#5)/8',
-                            'E4/8/r'
-                        ].join(','),
-                        { stem: 'down' }
-                    )
-                )
-            ]
-        })
+    measure()
+        .addNotes([
+            notes(
+                'B5/8/r',
+                '(E5 B5 E6)/8',
+                'B5/8/r',
+                '(C#5 F#5 C#6)/8',
+                'B5/8/r',
+                '(E5 B5 E6)/8',
+                'B5/8/r',
+                '(C#5 F#5 C#6)/8',
+                { stem: 'up' }
+            )
+        ], [
+            notes(
+                'E4/q/r',
+                '(C#4 F#4 C#5)/8',
+                'E4/8/r',
+                '(E4 B4 E5)/8',
+                'E4/8/r',
+                '(C#4 F#4 C#5)/8',
+                'E4/8/r',
+                { stem: 'down' }
+            )
+        ])
         .addClef('treble')
         .addTimeSignature('4/4');
 
     // Measure 2
-    system = vf.System({
-        x: 440,
-        y: 30,
-        width: 300,
-        spaceBetweenStaves: 10
-    });
+    measure()
+        .addNotes([
+            notes(
+                '(E4 B4 E5)/8',
+                'B4/8/r',
+                'B4/q/r',
+                'B4/h/r',
+                { stem: 'down' }
+            )
+        ]);
 
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    score.notes(
-                        [
-                            '(E4 B4 E5)/8',
-                            'B4/8/r',
-                            'B4/q/r',
-                            'B4/h/r',
-                        ].join(','),
-                        { stem: 'down' }
-                    )
-                )
-            ]
-        });
-
-    // Render the score
-    vf.draw();
+    render();
 })();
--- src/leitmotifs/bells-2/notation.js
+++ src/leitmotifs/bells-2/notation.js
@@ -1,49 +1,27 @@
 (function () {
-    let system;
-
-    // Configure the canvas
-    const vf = new Vex.Flow.Factory({
-        renderer: {
-            elementId: 'bells-2',
-            width: 422,
-            height: 120
-        }
+    const { score, measure, notes, render } = new VF.Sheet({
+        id: 'bells-2',
+        measureWidths: [420]
     });
 
-    // Setup the score
-    const score = vf.EasyScore();
     score.set({ time: '4/4' });
 
     // Measure 1
-    system = vf.System({
-        x: 0,
-        y: 0,
-        width: 420,
-        spaceBetweenStaves: 10
-    });
-
-    system
-        .addStave({
-            voices: [
-                score.voice(
-                    score.notes(
-                        [
-                            'B4/8/r',
-                            'G5/8',
-                            'B4/8/r',
-                            'C5/8',
-                            'B4/8/r',
-                            'D5/8',
-                            'B4/8/r',
-                            'F4/8'
-                        ].join(',')
-                    )
-                )
-            ]
-        })
+    measure()
+        .addNotes([
+            notes(
+                'B4/8/r',
+                'G5/8',
+                'B4/8/r',
+                'C5/8',
+                'B4/8/r',
+                'D5/8',
+                'B4/8/r',
+                'F4/8'
+            )
+        ])
         .addClef('treble')
         .addTimeSignature('4/4');
 
-    // Render the score
-    vf.draw();
+    render();
 })();
