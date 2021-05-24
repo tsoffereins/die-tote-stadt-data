d72fb60a5d6ed065da37df5dd1a4ae0d69ad5cf7
Thomas Offereins <thomas@mybit.nl>
2021-05-24 12:26:11 +0200

    Improve the notation for the motif 'vergangenheit'

--- src/leitmotifs/vergangenheit/notation.js
+++ src/leitmotifs/vergangenheit/notation.js
@@ -1,7 +1,7 @@
 (function () {
-    const { score, measure, notes, beam, render } = new VF.Sheet({
+    const { score, measure, notes, beam, curve, render } = new VF.Sheet({
         id: 'vergangenheit',
-        measureWidths: [280, 240]
+        measureWidths: [280, 260, 240]
     });
 
     score.set({ time: '4/4' });
@@ -9,13 +9,22 @@
     // Measure 1
     measure()
         .addNotes([
-            notes('B4/h/r', 'B4/q/r', 'B4/8/r'),
-            beam(notes('B4/16', 'F#4/16', { stem: 'up' }))
+            notes('B4/h/r', 'B4/q/r', 'B4/8/r', 'F#4/8')
         ])
         .addClef('treble')
         .addTimeSignature('4/4');
 
     // Measure 2
+    measure()
+        .addNotes([
+            notes('G#4/h/#1', 'G#4/q/#2', 'G#4/8/#3'),
+            beam(notes('B4/16', 'F#4/16', { stem: 'up' }))
+        ]);
+
+    curve(1, 2);
+    curve(2, 3);
+
+    // Measure 3
     measure()
         .addNotes([
             notes('E4/q.'),
