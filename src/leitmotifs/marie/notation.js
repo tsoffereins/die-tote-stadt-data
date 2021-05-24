77e8e3d124e12378e325f1606a86ee7798dad4d8
Thomas Offereins <thomas@mybit.nl>
2021-05-24 14:09:27 +0200

    Fix a note id issue in the motif of 'Marie'

--- src/leitmotifs/marie/notation.js
+++ src/leitmotifs/marie/notation.js
@@ -7,7 +7,7 @@
     // Measure 1
     measure('2/2')
         .addNotes([
-            tuplet(notes('F4/h', 'Eb4/q/#n1'), { num_notes: 3 }),
+            tuplet(notes('F4/h', 'Eb4/q/#1'), { num_notes: 3 }),
             tuplet(notes('E4/q/#n2', 'Db4/q', 'E4/q'))
         ])
         .addClef('treble')
@@ -18,8 +18,8 @@
     // Measure 2
     measure()
         .addNotes([
-            tuplet(notes('F4/q', 'Gb4/q', 'Ab4/q/#n3')),
-            tuplet(notes('Ab4/q/#n4', 'Bb4/q.', 'C5/8'), { num_notes: 3 })
+            tuplet(notes('F4/q', 'Gb4/q', 'Ab4/q/#3')),
+            tuplet(notes('Ab4/q/#4', 'Bb4/q.', 'C5/8'), { num_notes: 3 })
         ]);
 
     curve(3, 4);
@@ -27,8 +27,8 @@
     // Measure 3
     measure()
         .addNotes([
-            tuplet(notes('Db5/h', 'F5/q/#n5'), { num_notes: 3 }),
-            tuplet(notes('F5/q/#n6', 'D5/h'), { num_notes: 3 })
+            tuplet(notes('Db5/h', 'F5/q/#5'), { num_notes: 3 }),
+            tuplet(notes('F5/q/#6', 'D5/h'), { num_notes: 3 })
         ]);
 
     render();
