48a2e600134f7645f6f2c04cc5ee03a2778bf7ef
Thomas Offereins <thomas@mybit.nl>
2021-05-24 13:48:14 +0200

    Use the loud version of the 'Marietta' motif from the end of scene 4 as notation

--- src/leitmotifs/marietta/notation.js
+++ src/leitmotifs/marietta/notation.js
@@ -1,50 +1,51 @@
 (function () {
     const { score, measure, notes, tuplet, beam, articulate, render } = new VF.Sheet({
         id: 'marietta',
-        measureWidths: [540, 300],
-        height: 160,
-        offsetY: 20
+        measureWidths: [400, 200],
+        height: 140,
+        offsetY: 15
     });
 
-    score.set({ time: '3/2' });
+    score.set({ time: '4/4' });
 
     // Measure 1
     measure()
         .addNotes([
             tuplet(
-                notes('B4/8/r'),
-                beam(notes('A5/8/#1', 'G5/8/#2', 'A5/8/#3', 'D5/8/#4', 'E#5/8/#5')),
-                { num_notes: 3 }
+                notes(
+                    'B4/q/r',
+                    '(G#4 A#4 C#5 E5 G#5)/q/#1',
+                    '(F#4 A4 C5 E5 F#5)/q/#2'
+                ),
+                { num_notes: 3, y_offset: -10 }
             ),
             tuplet(
-                beam(notes('G4/8', 'A4/8')),
-                notes('E5/h'),
-                { num_notes: 3 }
-            ),
-            tuplet(
-                beam(notes('G4/8', 'A4/8')),
-                notes('G5/h'),
-                { num_notes: 3 }
-            ),
+                notes(
+                    '(G4 A4 C5 E5 G5)/q/#3',
+                    '(C#4 F4 A4 C#5)/q/#4',
+                    '(D#4 F4 A4 D#5)/q/#5'
+                ),
+                { num_notes: 3, y_offset: -10 }
+            )
         ])
         .addClef('treble')
-        .addKeySignature('B')
-        .addTimeSignature('3/4');
+        .addTimeSignature('4/4');
 
-    articulate([1, 2, 3, 4, 5], 'a.');
+    articulate([1, 2, 3, 4, 5], 'a>');
 
     // Measure 2
-    score.set({ time: '2/2' });
+    score.set({ time: '2/4' });
 
     measure()
         .addNotes([
             tuplet(
-                beam(notes('E#5/8', 'G5/8')),
-                notes('E#6/h'),
-                { num_notes: 3 }
-            ),
-            notes('B4/h/r')
+                beam(notes('F#4/8/#6', 'C#5/8/#7', { stem: 'down' })),
+                notes('(F#4 A4 C5 F#5)/h/#8'),
+                { num_notes: 3, y_offset: -10 }
+            )
         ]);
+
+    articulate([6, 7, 8], 'a>');
 
     render();
 })();
